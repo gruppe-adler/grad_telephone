87252fc772455d4880002424a60f259c37cad16b
simon.schmidt <nomisum@googlemail.com>
2021-11-20 12:08:04 +0100

    fixing macro includes

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,4 +1,5 @@
-#include "ace_macros.hpp"
+#include "\z\ace\addons\main\script_macros.hpp"
+#include <\a3\editor_f\Data\Scripts\dikCodes.h>
 
 #define KPATCH(PVAR) DOUBLES(PREFIX,PVAR)
 #define QKPATCH(PVAR) QUOTE(KPATCH(PVAR))
