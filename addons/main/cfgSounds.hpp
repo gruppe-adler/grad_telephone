35a835f4ae905e243f29416dd7befde062408b01
nomisum <nomisum@gmail.com>
2021-11-20 12:31:36 +0100

    sdf

--- addons/main/cfgSounds.hpp
+++ addons/main/cfgSounds.hpp
@@ -3,21 +3,21 @@ class cfgSounds {
     class grad_telephone_sound_hookStart
     {
         name = "hookStart";
-        sound[] = {"\grad_telephone\addons\sounds\hookStart.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\hookStart.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_hookEnd
     {
         name = "hookEnd";
-        sound[] = {"\grad_telephone\addons\sounds\hookEnd.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\hookEnd.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_hookRemote
     {
         name = "hookRemote";
-        sound[] = {"\grad_telephone\addons\sounds\hookRemote.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\hookRemote.ogg",0.75,1};
         titles[] = {};
     };
 
@@ -25,14 +25,14 @@ class cfgSounds {
     class grad_telephone_sound_phoneBeep1
     {
         name = "phoneBeep1";
-        sound[] = {"\grad_telephone\addons\sounds\phoneBeep1.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\phoneBeep1.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_phoneBeep2
     {
         name = "phoneBeep2";
-        sound[] = {"\grad_telephone\addons\sounds\phoneBeep2.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\phoneBeep2.ogg",0.75,1};
         titles[] = {};
     };
 
@@ -41,21 +41,21 @@ class cfgSounds {
     class grad_telephone_sound_phoneBusy1
     {
         name = "phoneBusy1";
-        sound[] = {"\grad_telephone\addons\sounds\phoneBusy1.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\phoneBusy1.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_phoneBusy2
     {
         name = "phoneBusy2";
-        sound[] = {"\grad_telephone\addons\sounds\phoneBusy2.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\phoneBusy2.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_phoneBusy3
     {
         name = "phoneBusy3";
-        sound[] = {"\grad_telephone\addons\sounds\phoneBusy3.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\phoneBusy3.ogg",0.75,1};
         titles[] = {};
     };
 
@@ -64,14 +64,14 @@ class cfgSounds {
     class grad_telephone_sound_phoneRing1
     {
         name = "phoneRing1";
-        sound[] = {"\grad_telephone\addons\sounds\phoneRing1.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\phoneRing1.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_phoneRing2
     {
         name = "phoneRing2";
-        sound[] = {"\grad_telephone\addons\sounds\phoneRing2.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\phoneRing2.ogg",0.75,1};
         titles[] = {};
     };
 
@@ -80,7 +80,7 @@ class cfgSounds {
     class grad_telephone_sound_phoneHangUp
     {
         name = "phoneHangUp";
-        sound[] = {"\grad_telephone\addons\sounds\phoneHangUp.ogg",2,1};
+        sound[] = {"x\grad_telephone\addons\sounds\phoneHangUp.ogg",2,1};
         titles[] = {};
     };
 
@@ -90,140 +90,140 @@ class cfgSounds {
     class grad_telephone_sound_rotary_0_down
     {
         name = "rotary_0_down";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_0_down.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_0_down.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_0_up
     {
         name = "rotary_0_up";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_0_up.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_0_up.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_1_down
     {
         name = "rotary_1_down";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_1_down.ogg",1.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_1_down.ogg",1.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_1_up
     {
         name = "rotary_1_up";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_1_up.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_1_up.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_2_down
     {
         name = "rotary_2_down";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_2_down.ogg",2.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_2_down.ogg",2.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_2_up
     {
         name = "rotary_2_up";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_2_up.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_2_up.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_3_down
     {
         name = "rotary_3_down";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_3_down.ogg",3.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_3_down.ogg",3.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_3_up
     {
         name = "rotary_3_up";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_3_up.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_3_up.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_4_down
     {
         name = "rotary_4_down";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_4_down.ogg",4.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_4_down.ogg",4.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_4_up
     {
         name = "rotary_4_up";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_4_up.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_4_up.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_5_down
     {
         name = "rotary_5_down";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_5_down.ogg",5.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_5_down.ogg",5.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_5_up
     {
         name = "rotary_5_up";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_5_up.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_5_up.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_6_down
     {
         name = "rotary_6_down";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_6_down.ogg",6.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_6_down.ogg",6.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_6_up
     {
         name = "rotary_6_up";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_6_up.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_6_up.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_7_down
     {
         name = "rotary_7_down";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_7_down.ogg",7.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_7_down.ogg",7.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_7_up
     {
         name = "rotary_7_up";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_7_up.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_7_up.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_8_down
     {
         name = "rotary_8_down";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_8_down.ogg",8.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_8_down.ogg",8.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_8_up
     {
         name = "rotary_8_up";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_8_up.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_8_up.ogg",0.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_9_down
     {
         name = "rotary_9_down";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_9_down.ogg",9.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_9_down.ogg",9.75,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_rotary_9_up
     {
         name = "rotary_9_up";
-        sound[] = {"\grad_telephone\addons\sounds\rotary_9_up.ogg",0.75,1};
+        sound[] = {"x\grad_telephone\addons\sounds\rotary_9_up.ogg",0.75,1};
         titles[] = {};
     };
 
@@ -233,14 +233,14 @@ class cfgSounds {
     class grad_telephone_sound_keypad_1
     {
         name = "keypad_1";
-        sound[] = {"\grad_telephone\addons\sounds\keypad_1.ogg",5,1};
+        sound[] = {"x\grad_telephone\addons\sounds\keypad_1.ogg",5,1};
         titles[] = {};
     };
 
     class grad_telephone_sound_keypad_2
     {
         name = "keypad_2";
-        sound[] = {"\grad_telephone\addons\sounds\keypad_2.ogg",5,1};
+        sound[] = {"x\grad_telephone\addons\sounds\keypad_2.ogg",5,1};
         titles[] = {};
     };
 
@@ -248,7 +248,7 @@ class cfgSounds {
     class grad_telephone_sound_keypad_3
     {
         name = "keypad_3";
-        sound[] = {"\grad_telephone\addons\sounds\keypad_3.ogg",5,1};
+        sound[] = {"x\grad_telephone\addons\sounds\keypad_3.ogg",5,1};
         titles[] = {};
     };
 
